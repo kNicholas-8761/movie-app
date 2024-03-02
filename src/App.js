273b63d8236a24828ac6bdc2c4c7f52f956c4321
kbrown <kamauri205@gmail.com>
2024-03-01 21:05:26 -0500

    dynamically update input value

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ function App() {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerms}
+          onChange={(e) => setSearchTerms(e.target.value)}
         ></input>
         <img
           src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
